e995902e040986de094d22407bfcbbf7ebac3b14
Vinicius <vsoares.a@gmail.com>
2021-04-05 01:41:12 -0300

    Resolução

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,20 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({title,value,type}: RequestDTO) : Transaction {
+
+    const {total} = this.transactionsRepository.getBalance();
+
+    if(type === 'outcome' && total < value){
+      throw Error('Need more income');
+    }
+
+    if(type != 'income' && type != 'outcome'){
+      throw Error('Invalid type');
+    }
+
+    const trans = this.transactionsRepository.create({title,value,type});
+    return trans;
   }
 }
 
